0c2e52a9e93cf8a04f8f6e5c9fb1ec8dccc9486e
IwanTz <iwansal64@gmail.com>
2024-03-08 13:15:06 +0700

    Initialize : Add clusters

---
+++ app/clusters/page.tsx
@@ -0,0 +1,45 @@
+"use server";
+import { connect_with_prisma, must_login } from "../server_utils";
+import styles from "./cluster.module.css";
+
+export default async function Clusters() {
+    const prisma = await connect_with_prisma();
+    const { id } = await must_login();
+
+    const results = await prisma.cluster.findMany({
+        where: {
+            OR: [
+                {
+                    creator_id: id,
+                },
+                {
+                    users: {
+                        every: {
+                            user: {
+                                id: id,
+                            },
+                        },
+                    },
+                },
+            ],
+        },
+    });
+
+    return (
+        <>
+            <div className={styles.sclusters}>
+                {results ? (
+                    results.map((value, index) => {
+                        return (
+                            <div className={styles.cluster} key={index}>
+                                <h1>{value.cluster_name}</h1>
+                            </div>
+                        );
+                    })
+                ) : (
+                    <h1>{"You Don't Have Any Clusters Yet"}</h1>
+                )}
+            </div>
+        </>
+    );
+}
