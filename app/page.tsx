b13b3962cda6fc9cbe0c877636a5f60b417c877c
IwanTz <iwansal64@gmail.com>
2024-03-09 11:06:04 +0700

    Fix must_login() issue on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,24 @@ import { Metadata } from "next";
 import styles from "./page.module.css";
 import Link from "next/link";
 import { must_login } from "./server-utils";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
     title: "INDATA: NextGen Data Management",
 };
 
 export default async function Home() {
-    must_login().then((user_data) => {});
-
     return (
         <>
             <Link href="clusters" className={styles.cluster}>
                 Clusters
             </Link>
+            <Suspense>
+                {(async () => {
+                    await must_login();
+                    return <></>;
+                })()}
+            </Suspense>
         </>
     );
 }
