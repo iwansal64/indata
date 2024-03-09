83a982beb930561d661b80a4e7ab36d9aa547b4b
IwanTz <iwansal64@gmail.com>
2024-03-09 11:04:00 +0700

    Add POST method to user API

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,8 @@
 import { check_authorization, connect_with_prisma } from "@/app/server-utils";
 import { NextRequest } from "next/server";
+import { user } from "@prisma/client";
+import { is_user } from "@/app/utils";
+import { validate_data } from "@/app/client-utils";
 
 export async function GET(request: NextRequest) {
     if (!(await check_authorization(request.headers))) {
@@ -32,5 +35,64 @@ export async function GET(request: NextRequest) {
         success: true,
         total: user_results.length,
         results: user_results,
+    });
+}
+
+export async function POST(request: NextRequest) {
+    if (!(await check_authorization(request.headers))) {
+        return new Response("Unauthorized Request!", {
+            status: 401,
+        });
+    }
+
+    const request_body = await request.json();
+
+    if (!Object.keys(request_body).includes("new_data")) {
+        return new Response("Body params not complete!", {
+            status: 400,
+        });
+    }
+
+    const user_data = request_body["new_data"];
+    if (!is_user(user_data)) {
+        return new Response("Body new_data param is not correct!", {
+            status: 400,
+        });
+    }
+
+    const prisma = await connect_with_prisma();
+
+    //? Check if the account is already inside the database
+    const user_result = await prisma.user.findFirst({
+        where: {
+            OR: [
+                {
+                    name: user_data.name,
+                },
+                {
+                    email: user_data.email,
+                },
+            ],
+        },
+    });
+
+    if (user_result) {
+        return new Response("Account is already created before!", {
+            status: 400,
+        });
+    }
+
+    //? Creating data inside the database
+    const prisma_result = await prisma.user.create({
+        data: {
+            name: user_data.name,
+            pass: user_data.pass,
+            email: user_data.email,
+        },
+    });
+
+    return Response.json({
+        success: true,
+        results: prisma_result,
     });
 }
